557aeffd51c19b73c820612edeab131df37cae7c
mrcbns <moracabanas@live.com>
2020-05-06 12:02:44 +0200

    initial working uncomplete GUI

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 from tkinter import *
 
-root = Tk()
+root = Tk() 
 
-country = "es"
+Label(root, text="Insert your country code ie: US").pack()
+country = Entry(root)
+country.pack()
 ssid = "2.4Ghz"
 password = "4058520201"
 
+def write_file(lines, filename):
 
-supplicant = [
-    f'country={country}\n',
-    'update_config=1\n',
-    'ctrl_interface=/var/run/wpa_supplicant\n',
-    '\n'
-    'network{\n',
-    'scan_ssid=1\n',
-    f'ssid="{ssid}"\n',
-    f'psk="{password}"\n',
-    '}'
-]
-
-def file_write():
-
-    f =  open("wpa_supplicant.conf", "w+")
-    f.writelines(supplicant)
+    f =  open(filename, "w+")
+    f.writelines(lines)
     f.close()
-    myLabel = Label(root, text="File created!")
+    myLabel = Label(root, text=f'{filename} created')
     myLabel.pack()
 
-myButton = Button(root, text="Generate", command=file_write)
-myButton.pack()
+
+def update_write():
+    #first update array
+    supplicant = [
+        f'country={country.get()}\n',
+        'update_config=1\n',
+        'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n',
+        '\n'
+        'network={\n',
+        #'scan_ssid=1\n',
+        f'  ssid="{ssid}"\n',
+        f'  psk="{password}"\n',
+        '}'
+    ]
+
+    #call the write function
+    write_file(supplicant, "wpa_supplicant.conf")
+
+
+genButton = Button(root, text="Generate Files", command=lambda: update_write())
+genButton.pack()
 
 root.mainloop()
