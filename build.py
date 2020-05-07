e864cc5d735a62cedcc93c89807c1c003a842ca7
mrcbns <moracabanas@live.com>
2020-05-07 02:14:38 +0200

    updated build name and OS agnostic script

--- build.py
+++ build.py
@@ -1,3 +1,12 @@
 import os
+import platform
 
-os.system("pyinstaller --onefile --noconsole --icon=icon.ico main.py --add-data icon.ico;.")
+oper = platform.system()
+
+if(oper == "Windows"):
+    ico_cmd = "icon.ico;."
+else:
+    ico_cmd = "icon.ico:."
+
+
+os.system(f"pyinstaller --onefile --noconsole --name=HeadlessGenerator --icon=icon.ico main.py --add-data {ico_cmd}")
